d8cf19b9f2f36b3a82965eec0ddc4a3d37638157
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-12-11 20:06:17 -0300

    fix: timeout error and not used variables

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,13 +1,8 @@
 import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 import { CurrentSearch } from './current-search'
 
 export default function SearchLoading() {
-  const searchParams = useSearchParams()
-
-  const query = searchParams.get('q')
-
   return (
     <div className="flex flex-col gap-4">
       <Suspense fallback={null}>
